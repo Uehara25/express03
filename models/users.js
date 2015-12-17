67d95f0e87054cfeb2fd4db331b17bd841c27a54
Uehara25 <akiya.noface@gmail.com>
2015-12-17 09:20:10 +0900

    added users.js

---
+++ models/users.js
@@ -0,0 +1,56 @@
+var crypto = require('crypto');
+var database = require('./database');
+var db = database.createClient();
+var users = exports;
+
+users.authenticate = function (name, password, callback) {
+  db.query('SELECT * FROM users WHERE name = ?', [name,], queryCallBack);
+  function queryCallback(err, results, fields) {
+    db.end();
+    if (err) {
+      callback(err);
+      return;
+    }
+    if (results && (results.length > 0)) {
+      userInfo = results[0];
+      if (userInfo.password == _hashPassword(password)){
+        delete userInfo.password;
+        callback(null, userInfo);
+        return;
+      }
+    }
+    // if no user is corresponded
+    callback(err, null);
+    return;
+  }
+}
+
+// create password hash
+function _hashPassword(password) {
+  if (password === '') {
+    return '';
+  }
+  var shasum = crypto.createHash('sha256');
+  shasum.update(password);
+  return shasum.digest('hex');
+}
+
+users.CreateUser = function (name, password, callback) {
+  var hashedPassword = _hashPassword(password);
+  db.query(
+    'INSERT INTO users'
+     + '(uid, name, password)'
+     + 'VALUES'
+     + '(NULL, ?, ?)'
+     + ';',
+     [name, hashedPassword],
+     function (err, results, fields) {
+       db.end();
+       var sid = results.insertId;
+       if (err) {
+         callback(new Error('Insert failed.'));
+       }
+       callback(null);
+     }
+  )
+}
