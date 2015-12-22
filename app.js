5ea27715c848233aa72791aa097a48015ee1346a
Uehara25 <akiya.noface@gmail.com>
2015-12-22 09:12:33 +0900

    add requirements

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var MemchachedStore = require('connect-memcached');
+
+var config = require('./config');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
